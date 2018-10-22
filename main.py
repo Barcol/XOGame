d40d7909a4b71078c2e52cd439bf095187e9a932
radek <barcol1@interia.pl>
2018-10-22 20:45:54 +0200

    coder added, cosmetic changes

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import socket
 from typing import Union, List
 
 
-class Player():  # zadania tej klasy zostaly mocno obciete. W zasadzie to pozostaly tylko 3.
+class Player:  # zadania tej klasy zostaly mocno obciete. W zasadzie to pozostaly tylko 3.
     def __init__(self, mark: str):  # po pierwsze sprawdza czy ma jeden znak
         if len(mark) != 1:
-            #raise PlayerMarkError("Symbol gracza musi mieć jeden znak!")
+            # raise PlayerMarkError("Symbol gracza musi mieć jeden znak!")
             pass
         self.__mark = mark
         self.__adress = str(input("Podaj adres IP gracza ".format(self.__mark)))
@@ -20,9 +20,9 @@ class Player():  # zadania tej klasy zostaly mocno obciete. W zasadzie to pozost
         return self.__mark
 
 
-class NetPlay():
-    def __init__(self, player: Player):  # konstruktor jedynie przypisuje adres klienta do obiektu ktory odpowiada za laczenie
-        self.__adress = player.get_adress  # na kazdego gracza przypada jeden obiekt tej klasy, ktory potrafi sie laczyc TYLKO z nim
+class NetPlay:
+    def __init__(self, player: Player):  # konstruktor jedynie przypisuje adres klienta
+        self.__adress = player.get_adress  # na kazdego gracza przypada jeden obiekt tej klasy
         self.__port = 1234  # port jest jeden dla kazdego
 
     def connect_to_player(self):  # natomiast za laczenie odpowiada ta funckja (wywolujemy przed kazdym rchem gracza!)
@@ -30,18 +30,18 @@ class NetPlay():
         self.__sock.bind((self.__adress, self.__port))
         self.__sock.listen(1)
         self.__conn, self.__addr = self.__sock.accept()
-        print ("polaczylem sie! typek to".format(self.__addr))
-        print ("test")
+        print("polaczylem sie! typek to".format(self.__addr))
+        print("test")
 
     def draw_map(self, ready_board):
-        self.__conn.sendall(bytes(len(ready_board)))  # wysylamy klientowi wyosoksc tabeli, zbey wiedzial ile wersow sie spodziewac
+        self.__conn.sendall(self.coder(len(ready_board)))  # wysylamy klientowi wysoksc tabeli
         for row in ready_board:  # dla każdego wiersza w tabeli
-            self.__conn.sendall(bytes(" ".join(row), "utf-8"))  # wyslij go jako string
+            self.__conn.sendall(self.coder(" ".join(row)))
 
     def do_move(self):  # po wyswietleniu klientowi obecnego stanu rzeczy, pytamy go o wspolrzedne
-        self.__conn.sendall(bytes("Podaj X", "utf-8"))
+        self.__conn.sendall(self.coder("Podaj X"))
         x = self.__conn.recv(1024)
-        self.__conn.sendall(bytes("Podaj Y", "utf-8"))
+        self.__conn.sendall(self.coder("Podaj Y"))
         y = self.__conn.recv(1024)
         return x, y  # funkcja zwraca co zdecydowal gracz. rola obiektu zostala zakonczona
 
@@ -49,14 +49,26 @@ class NetPlay():
         self.__conn.close()
 
     def tell_who_won(self, mark: str):
-        self.__conn.sendall(bytes("GRA SKONCZONA. {} WYGRAL!".format(mark), "utf-8"))
+        self.__conn.sendall(self.coder("GRA SKONCZONA. {} WYGRAL!".format(mark)))
+
+    def coder(self, data: Union[int, str, bytes]):
+        if not isinstance(data, bytes):
+            return bytes(data, "utf-8")
+        if bytes == "":
+            return 1
+        if isinstance(data, bytes):
+            data = str(data, "utf-8")
+            if data.isdecimal():
+                return int(data)
+            else:
+                return data
 
     @property
     def get_mark(self):
         return player.get_mark
 
 
-class Board():  # ta klasa nie zmienila sie za wiele
+class Board:  # ta klasa nie zmienila sie za wiele
     def __init__(self, height: int, width: int):
         self.__height = height
         self.__width = width
@@ -113,31 +125,27 @@ class Board():  # ta klasa nie zmienila sie za wiele
         return self.__board
 
 
-
-class XOGame():
+class XOGame:
     def __init__(self, board: Board, connection_table: List[NetPlay]):
         self.__board = board
         self.__connection_table = connection_table
 
-    def play_round(self) -> Union[
-        str, bool]:  # funkcja robi standardowy zestaw czynnosci potrzebny do rozgerania tury jednego gracza
+    def play_round(self) -> Union[str, bool]:  # funkcja robi standardowy zestaw czynnosci potrzebny do rozgerania tury
         for player in self.__connection_table:
             player.connect_to_player()
             player.draw_map(self.__board.show_board())
             x, y = player.do_move()
-            print (x)
-            print (y)
-            try :self.__board.put_mark_if_possible(player.get_mark, int(x, base =2), int(y, base =2))
-            except: pass
+            print(x)
+            print(y)
+            self.__board.put_mark_if_possible(player.get_mark, int(x, base=2), int(y, base=2))
             player.end_connection()
             return board.check_win()
 
     def tell_who_won(self, mark: str):
-        player: NetPlay
         for player in self.__connection_table:
             player.connect_to_player()
             player.tell_who_won(mark)
-            player.end_connection
+            player.end_connection()
 
     def is_board_full(self) -> bool:
         return board.is_board_full()
@@ -151,7 +159,7 @@ if __name__ == '__main__':  # gra nie ruszy jeżeli nie jest mainem
     player_list = [Player("X"), Player("O")]  # tutaj można dopisać nowych graczy
     for player in player_list:  # i dla każdego z nich
         connection_table.append(NetPlay(player))  # tworzony jest nowy obiekt do obslugi socketa
-    game = XOGame(board, connection_table)  # a ponieważ te obiekty zawieraja w sobie playera, to do gry wchodza juz same one, a nie player_list
+    game = XOGame(board, connection_table)  # to do gry wchodza juz same one, a nie player_list
 
     while not game.is_board_full():
         winning_symbol = game.play_round()
