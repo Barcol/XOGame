0accb5cfdd3c10bfb648a3c2332519fd3fe4a098
xaaq <xaaq333@gmail.com>
2018-10-27 20:12:24 +0200

    do some stuff

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+from enum import Enum
 from typing import Union
 
 host = "127.0.0.1"  # str(input("podaj adres IP serwera"))
@@ -7,38 +8,54 @@ port = 1234
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 flag = 0
 
-
-def coder(data: Union[int, str, bytes]):
-    if not isinstance(data, bytes):
-        return bytes(str(data), "utf-8")
-    if bytes == "":
-        return 1
-    if isinstance(data, bytes):
-        data = str(data, "utf-8")
-        if data.isdecimal():
-            return int(data)
-        else:
-            return data
+class MessageType(Enum):
+    BOARD_SEND = 0
+    X_REQUEST = 1
+    Y_REQUEST = 2
+    X_RESPONSE = 3
+    Y_RESPONSE = 4
 
 
-while True:
-    if flag == 0:
-        sock.connect((host, port))
-        flag = 1
-        print("polaczylem sie z {}".format(host))
-    received = sock.recv(1024)
-    if coder(received) == "Podaj X":
-        move = int(input("podaj X"))
-        sock.sendall(coder(move))
-    if coder(received) == "Podaj Y":
-        move = int(input("podaj Y"))
-        sock.sendall(coder(move))
-        flag = 0
-    if isinstance(received, int):
-        for table_row_number in range(received):
-            table_row = sock.recv(1024)
-            print(coder(table_row))
-    if str(received) == "GRA":  # ma sie zaczynac od tego, a nie byc tym! wyrazenie regularne needed
-        print(received)
-        break
-print("Zapraszam ponownie")
+# def coder(data: Union[int, str, bytes]):
+#     if not isinstance(data, bytes):
+#         return bytes(str(data), "utf-8")
+#     if bytes == "":
+#         return 1
+#     if isinstance(data, bytes):
+#         data = str(data, "utf-8")
+#         if data.isdecimal():
+#             return int(data)
+#         else:
+#             return data
+
+
+sock.connect((host, port))
+
+try:
+    while 1:
+        a = sock.recv(1024)
+        if a:
+            print(a.decode())
+
+except KeyboardInterrupt:
+    sock.close()
+# while True:
+#     # if flag == 0:
+#     #     flag = 1
+#     #     print("polaczylem sie z {}".format(host))
+#     received = sock.recv(1024)
+#     if coder(received) == "Podaj X":
+#         move = int(input("podaj X"))
+#         sock.sendall(coder(move))
+#     if coder(received) == "Podaj Y":
+#         move = int(input("podaj Y"))
+#         sock.sendall(coder(move))
+#         flag = 0
+#     if isinstance(received, int):
+#         for table_row_number in range(received):
+#             table_row = sock.recv(1024)
+#             print(coder(table_row))
+#     if str(received) == "GRA":  # ma sie zaczynac od tego, a nie byc tym! wyrazenie regularne needed
+#         print(received)
+#         break
+# print("Zapraszam ponownie")
--- main.py
+++ main.py
@@ -1,55 +1,73 @@
+import pickle
 import re
 import socket
-from typing import Union, List
+from enum import Enum
+from typing import Union, List, Any, Tuple
 
 
-class Player:
-    def __init__(self, mark: str):
-        if len(mark) != 1:
-            # raise PlayerMarkError("Symbol gracza musi mieć jeden znak!")
-            pass
-        self.__mark = mark
-        self.__adress = "127.0.0.1"  # str(input("Podaj adres IP gracza ".format(self.__mark)))
-
-    @property
-    def get_adress(self):  # po drugie chwali sie swoim adresem
-        return self.__adress
-
-    @property
-    def get_mark(self):  # po trzecie chwali sie swoim znakiem
-        return self.__mark
+class MessageType(Enum):
+    BOARD_SEND = 0
+    X_REQUEST = 1
+    Y_REQUEST = 2
+    X_RESPONSE = 3
+    Y_RESPONSE = 4
 
 
-class NetPlay:
-    def __init__(self, netplayer: Player):  # konstruktor jedynie przypisuje adres klienta
-        self.__adress = netplayer.get_adress  # na kazdego gracza przypada jeden obiekt tej klasy
-        self.__port = 1234  # port jest jeden dla kazdego
-        self.__netplayer = netplayer
+# class Player:
+#     def __init__(self, mark: str):
+#         if len(mark) != 1:
+#             # raise PlayerMarkError("Symbol gracza musi mieć jeden znak!")
+#             pass
+#         self.__mark = mark
+#         self.__address = "127.0.0.1"  # str(input("Podaj adres IP gracza ".format(self.__mark)))
+#
+#     @property
+#     def address(self):  # po drugie chwali sie swoim adresem
+#         return self.__address
+#
+#     @property
+#     def mark(self):  # po trzecie chwali sie swoim znakiem
+#         return self.__mark
 
-    def connect_to_player(self):  # natomiast za laczenie odpowiada ta funckja (wywolujemy przed kazdym rchem gracza!)
-        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__sock.bind((self.__adress, self.__port))
-        self.__sock.listen(1)
-        self.__conn, self.__addr = self.__sock.accept()
-        print("polaczylem sie! typek to{}".format(self.__addr))
 
-    def draw_map(self, ready_board):
-        self.__conn.sendall(self.coder(len(ready_board)))  # wysylamy klientowi wysoksc tabeli
-        for row in ready_board:  # dla każdego wiersza w tabeli
-            self.__conn.sendall(self.coder(" ".join(row)))
+class NetPlayer:
+    def __init__(self, connection: socket):  # konstruktor jedynie przypisuje adres klienta
+        # self.__port = 1234  # port jest jeden dla kazdego
+        # self.__net_player = net_player
+        # self.__sock = None
+        # self.__connection = None
+        self.__connection = connection
 
-    def do_move(self):  # po wyswietleniu klientowi obecnego stanu rzeczy, pytamy go o wspolrzedne
-        self.__conn.sendall(self.coder("Podaj X"))
-        x = self.coder(self.__conn.recv(1024))
-        self.__conn.sendall(self.coder("Podaj Y"))
-        y = self.coder(self.__conn.recv(1024))
-        return int(x), int(y)
+    # def connect_to_player(self):  # natomiast za laczenie odpowiada ta funckja (wywolujemy przed kazdym rchem gracza!)
+    #     self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     self.__sock.bind((self.__net_player.address, self.__port))
+    #     self.__sock.listen(1)
+    #     self.__connection, address = self.__sock.accept()
+    #     print("polaczylem sie! typek to{}".format(address))
+
+    def send_board(self, string_board: str):
+        self.__connection.send(string_board.encode())
+
+    def get_move_coordinates(self) -> Tuple[int, int]:
+        # po wyswietleniu klientowi obecnego stanu rzeczy, pytamy go o wspolrzedne
+        x = self.__send_move_request(MessageType.X_REQUEST, MessageType.X_RESPONSE)
+        y = self.__send_move_request(MessageType.Y_REQUEST, MessageType.Y_RESPONSE)
+        return x, y
 
     def end_connection(self):  # konczy polaczenie
-        self.__conn.close()
+        self.__connection.close()
 
     def tell_who_won(self, mark: str):
-        self.__conn.sendall(self.coder("GRA SKONCZONA. {} WYGRAL!".format(mark)))
+        self.__connection.sendall(self.coder("GRA SKONCZONA. {} WYGRAL!".format(mark)))
+
+    def __send_move_request(self, request_type: MessageType, response_type: MessageType) -> int:
+        response = (None, None)
+
+        while response[0] != response_type:
+            self.__connection.send(pickle.dumps((request_type, None)))
+            response = pickle.loads(self.__connection.recv(1024))
+
+        return response[1]
 
     @staticmethod
     def coder(data: Union[int, str, bytes]):
@@ -68,15 +86,42 @@ class NetPlay:
                 return data
 
     @property
-    def get_mark(self):
-        return self.__netplayer.get_mark
+    def mark(self):
+        return self.__net_player.mark
+
+
+class Server:
+    def __init__(self):
+        self.__socket = self.__create_socket("127.0.0.1", 1234)
+
+    def wait_for_players(self, number_of_players: int) -> List[NetPlayer]:
+        return [NetPlayer(self.__socket.accept()[0]) for _ in range(number_of_players)]
+
+        # self.__socket.sendall(board.encode())
+        # self.__connection_table.append(connection)
+        # print("jest! mamy gnoja! a imie jego: {}".format(address))
+
+    def close_socket(self):
+        self.__socket.close()
+
+    @staticmethod
+    def __create_socket(hostname: str, port: int):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((hostname, port))
+        sock.listen(2)
+        return sock
+
+
+# class MessagePacker:
+#     @staticmethod
+#     def pack_message(message_type: MessageType, data: Any) -> Tuple[int, str]:
+# return
 
 
 class Board:  # ta klasa nie zmienila sie za wiele
     def __init__(self, board_height: int, board_width: int):
-        self.__height = board_height
-        self.__width = board_width
-        self.__board = [["-" for _ in range(self.__width)] for _ in range(self.__height)]
+        self.__board = [["-" for _ in range(board_width)] for _ in range(board_height)]
 
     def check_win(self) -> Union[str, bool]:
         checked_wins = [self.__check_all_win_vectors(0, 1),
@@ -115,9 +160,7 @@ class Board:  # ta klasa nie zmienila sie za wiele
         return True
 
     def __are_coordinates_in_board_range(self, x: int, y: int) -> bool:
-        if (x <= self.__width) and (y <= self.__height):
-            return True
-        return False
+        return (x <= self.__width) and (y <= self.height)
 
     def put_mark_if_possible(self, mark: str, x: int, y: int) -> bool:
         if self.__are_coordinates_in_board_range and (self.__board[x][y] == "-"):
@@ -125,52 +168,69 @@ class Board:  # ta klasa nie zmienila sie za wiele
             return True
         return False
 
-
-    def show_board(self):
-        return self.__board
+    def show_board(self) -> str:
+        string_board = "\n".join([" ".join(row) for row in self.__board])
+        return string_board
 
 
 class XOGame:
-    def __init__(self, play_board: Board, connect_table: List[NetPlay]):
+    def __init__(self, play_board: Board, connection_table: List[NetPlayer]):
+        # TODO: zrobic zeby NetPlayer i np. jakis ComputerPLayer mialy interfejs (i stworzyc ComputerPLayer)
         self.__board = play_board
-        self.__connection_table = connect_table
+        self.__player_list = connection_table
+        # self.__server = server
 
-    def play_round(self) -> Union[str, bool]:  # funkcja robi standardowy zestaw czynnosci potrzebny do rozgerania tury
-        for actual_player in self.__connection_table:
-            actual_player.connect_to_player()
-            actual_player.draw_map(self.__board.show_board())
-            x, y = actual_player.do_move()
-            print(x)
-            print(y)
-            self.__board.put_mark_if_possible(player.get_mark, x, y)
-            actual_player.end_connection()
-            return board.check_win()
+    def play_round(self):  # funkcja robi standardowy zestaw czynnosci potrzebny do rozgerania tury
+        for player in self.__player_list:
+            self.__send_board_to_all(self.__board.show_board())
+            x, y = player.get_move_coordinates()
+            self.__board.put_mark_if_possible(player.mark, x, y)
+
+        # for actual_player in self.__connection_table:
+        #     actual_player.send_board(self.__board.show_board())
+        #     x, y = actual_player.do_move()
+        #     print(x)
+        #     print(y)
+        #     self.__board.put_mark_if_possible(actual_player.mark, x, y)
+        #     actual_player.end_connection()
+        # return self.__board.check_win()
 
     def tell_who_won(self, mark: str):
-        for actual_player in self.__connection_table:
+        for actual_player in self.__player_list:
             actual_player.connect_to_player()
             actual_player.tell_who_won(mark)
             actual_player.end_connection()
 
     def is_board_full(self) -> bool:
-        return board.is_board_full()
+        return self.__board.is_board_full()
+
+    def __send_board_to_all(self, string_board: str):
+        for connection in self.__player_list:
+            connection.send_board(string_board)
 
 
 if __name__ == '__main__':  # gra nie ruszy jeżeli nie jest mainem
-    connection_table = []
     width = 5  # int(input("Podaj szerokosc"))
     height = 4  # int(input("podaj wysokosc"))
     board = Board(width, height)
-    player_list = [Player("X"), Player("O")]  # tutaj można dopisać nowych graczy
-    for player in player_list:  # i dla każdego z nich
-        connection_table.append(NetPlay(player))  # tworzony jest nowy obiekt do obslugi socketa
-    game = XOGame(board, connection_table)  # to do gry wchodza juz same one, a nie player_list
+    # player_list = [Player("X"), Player("O")]  # tutaj można dopisać nowych graczy
+    server = Server()
+    player_list = server.wait_for_players(1)
+    # connection_table = [NetPlay(player) for player in player_list]
+    try:
+        game = XOGame(board, player_list)  # to do gry wchodza juz same one, a nie player_list
+        game.play_round()
+    except Exception:
+        server.close_socket()
+    finally:
+        server.close_socket()
 
-    while not game.is_board_full():
-        winning_symbol = game.play_round()
-        if winning_symbol:
-            game.tell_who_won(winning_symbol)  # jeżeli ktos wygra to obaj gracze dostana o tym infromacje
-            break
-    if game.is_board_full():
-        game.tell_who_won("NIKT NIE")  # analogicznie jezeli nikt nie wygra
+    #
+    # while not game.is_board_full():
+    #     winning_symbol = game.play_round()
+    #     if winning_symbol:
+    #         game.tell_who_won(winning_symbol)  # jeżeli ktos wygra to obaj gracze dostana o tym infromacje
+    #         break
+    # if game.is_board_full():
+    #     game.tell_who_won("NIKT NIE")  # analogicznie jezeli nikt nie wygra
 print("Gra zakonczona.")
